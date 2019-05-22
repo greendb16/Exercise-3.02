169e466d1817a1ae95c10f10fa8b98977d692378
Greendb16 <greendb16@gmail.com>
2019-05-22 10:49:57 -0400

    Completed Exercise 3.02

---
+++ src/main/java/com/example/demo/Todo.java
@@ -0,0 +1,62 @@
+package com.example.demo;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.Date;
+
+@Entity
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotNull
+    @Size(min = 3, max = 25)
+    private String name;
+
+    @NotNull
+    @Size(min=4, max=15)
+    private String duedate;
+
+    @NotNull
+    @Size(min = 1, max = 10)
+    private String priority;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDuedate() {
+        return duedate;
+    }
+
+    public void setDuedate(String duedate) {
+        this.duedate = duedate;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+}
